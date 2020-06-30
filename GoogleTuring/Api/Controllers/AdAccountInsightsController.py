9f70f47211d73740e30bcf4c1814ed9dd7706946
Alin Mihaila <Alin@filed.com>
2020-06-30 10:13:40 +0000

    Merged PR 4807: JWT fix
    jwt added

--- GoogleTuring/Api/Controllers/AdAccountInsightsController.py
+++ GoogleTuring/Api/Controllers/AdAccountInsightsController.py
@@ -1,4 +1,5 @@
 import json
+
 import humps
 from flask import request, Response
 from flask_jwt_simple import jwt_required, get_jwt
@@ -7,22 +8,22 @@ from flask_restful import Resource
 from Core.Web.FacebookGraphAPI.Tools import Tools
 from Core.Web.Security.JWTTools import extract_business_owner_google_id
 from GoogleTuring.Api.Commands.AdAccountInsightsCommand import AdAccountInsightsCommand
-from GoogleTuring.Api.Mappings.AdAccountInsightsCommandMapping import AdAccountInsightsCommandMapping
 from GoogleTuring.Api.CommandsHandlers.GoogleAdAccountInsightsHandler import \
     GoogleAdAccountInsightsHandler
+from GoogleTuring.Api.Mappings.AdAccountInsightsCommandMapping import AdAccountInsightsCommandMapping
 from GoogleTuring.Api.Startup import startup
 
 
 class AdAccountInsightsEndpoint(Resource):
 
-    # @jwt_required
+    @jwt_required
     def post(self):
         try:
-            # business_owner_google_id = extract_business_owner_google_id(get_jwt())
+            business_owner_google_id = extract_business_owner_google_id(get_jwt())
             raw_request = humps.decamelize(request.get_json(force=True))
             mapping = AdAccountInsightsCommandMapping(AdAccountInsightsCommand)
             command = mapping.load(raw_request)
-            # command.business_owner_google_id = business_owner_google_id
+            command.business_owner_google_id = business_owner_google_id
         except Exception as e:
             response = json.dumps({"message": f"Failed to process request. Error {str(e)}"})
             return Response(response=response, status=400, mimetype='application/json')
