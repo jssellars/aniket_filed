c1174ccc1f98b2476dcc7f77c6ac58af4fc73d77
Alin Mihaila <Alin@filed.com>
2020-07-01 13:12:20 +0000

    Merged PR 4821: Reduced request time
    reduced request time
    https://filed.atlassian.net/browse/UH2020-1569

--- GoogleTuring/Api/CommandsHandlers/GoogleAdAccountInsightsHandler.py
+++ GoogleTuring/Api/CommandsHandlers/GoogleAdAccountInsightsHandler.py
@@ -1,4 +1,7 @@
+import math
 from datetime import datetime
+from queue import Queue
+from threading import Thread
 
 from flask import make_response, jsonify
 
@@ -12,6 +15,8 @@ from GoogleTuring.Infrastructure.PersistenceLayer.GoogleBusinessOwnerMongoReposi
 
 
 class GoogleAdAccountInsightsHandler(GoogleTokenGetter):
+    __BATCH_SIZE_PERCENT = 0.2
+
     @classmethod
     def __get_all_client_ids_for_business_owner(cls, business_owner_google_id):
         mongo_conn_handler = MongoConnectionHandler(startup.mongo_config)
@@ -31,12 +36,42 @@ class GoogleAdAccountInsightsHandler(GoogleTokenGetter):
             client_customer_ids = cls.__get_all_client_ids_for_business_owner(command.business_owner_google_id)
             start_date = datetime.strptime(command.from_date, '%Y-%m-%d')
             end_date = datetime.strptime(command.to_date, '%Y-%m-%d')
-            response = AdWordsAPIAdAccountInsightsHandler.get_ad_account_insights(
-                config=config,
-                permanent_token=business_owner_permanent_token,
-                client_customer_ids=client_customer_ids,
-                start_date=start_date,
-                end_date=end_date)
+
+            number_of_customers = len(client_customer_ids)
+            batch_size = math.ceil(cls.__BATCH_SIZE_PERCENT * number_of_customers)
+            queue = Queue()
+            worker_threads = []
+            response = []
+
+            for start in range(0, number_of_customers, batch_size):
+                if start + batch_size < number_of_customers:
+                    client_customer_ids_slice = client_customer_ids[start:start + batch_size]
+                else:
+                    client_customer_ids_slice = client_customer_ids[start:]
+
+                t = Thread(target=lambda q, config, permanent_token, client_customer_ids, start_date, end_date: q.put(
+                    AdWordsAPIAdAccountInsightsHandler.get_ad_account_insights(config=config,
+                                                                               permanent_token=permanent_token,
+                                                                               client_customer_ids=client_customer_ids,
+                                                                               start_date=start_date,
+                                                                               end_date=end_date)),
+                           args=(queue,
+                                 config,
+                                 business_owner_permanent_token,
+                                 client_customer_ids_slice,
+                                 start_date,
+                                 end_date))
+                worker_threads.append(t)
+
+            for worker_thread in worker_threads:
+                worker_thread.start()
+
+            for worker_thread in worker_threads:
+                worker_thread.join()
+
+            while not queue.empty():
+                response.extend(queue.get())
+
             return response
         else:
             return make_response(jsonify(error_message="Google account not found"), 404)
