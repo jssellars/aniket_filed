a4fb938cc8320578dfee25a2d090b8cc28c440cc
Andrei Luchici <andrei@filed.com>
2020-07-28 15:27:44 +0000

    Merged PR 5009: bug fix - filtered structures

--- FacebookTuring/Api/CommandsHandlers/AdsManagerFilteredStructuresCommandHandler.py
+++ FacebookTuring/Api/CommandsHandlers/AdsManagerFilteredStructuresCommandHandler.py
@@ -23,7 +23,8 @@ class AdsManagerFilteredStructuresCommandHandler:
                                                               campaign_ids=command.campaign_ids,
                                                               adset_ids=command.adset_ids,
                                                               statuses=command.statuses)
-
+            if not response:
+                return
             mapping = AdsManagerStructureMinimalMapping(level=collection_name)
             response = mapping.load(response, many=True)
             return response
