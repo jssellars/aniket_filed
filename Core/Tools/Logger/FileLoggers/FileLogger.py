e5db77685770b876019adca0d222694898eaa54c
Alin Mihaila <Alin@filed.com>
2020-09-08 14:29:20 +0000

    Merged PR 5180: fix log filename strategy
    fix log filename strategy

--- Core/Tools/Logger/FileLoggers/FileLogger.py
+++ Core/Tools/Logger/FileLoggers/FileLogger.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import typing
-from datetime import datetime
 from logging.handlers import TimedRotatingFileHandler
 
 from Core.Tools.Logger.FileLoggers.CustomJsonFormatter import CustomJsonFormatter
@@ -10,7 +9,6 @@ from Core.Tools.Logger.LoggingLevelEnum import LoggingLevelEnum
 
 class FileLogger:
     LOGS_FOLDER = 'logs'
-    DAY = 24 * 3600
     _instance = None
 
     def __new__(self,
@@ -23,28 +21,26 @@ class FileLogger:
 
             self.name = name
             self.level = LoggingLevelEnum.get_enum_by_name(level).value
-            self.file_name = os.path.join(self.LOGS_FOLDER, index_name + datetime.now().strftime("%Y-%m-%d") + ".log")
+            self.file_name = os.path.join(self.LOGS_FOLDER, index_name)
 
-            if not os.path.exists(self.LOGS_FOLDER):
-                os.makedirs(self.LOGS_FOLDER)
+            os.makedirs(self.LOGS_FOLDER, exist_ok=True)
 
             self.logger = FileLogger.init_logger(self)
 
         return self._instance
 
     def init_logger(self):
-        # define logs format
-        formatter = CustomJsonFormatter('(level):(name)')
-
         # create a file handler to log to file
-        handler = TimedRotatingFileHandler(self.file_name,
-                                           when='midnight', interval=self.DAY)
+        suffixed_file_name = f"{self.file_name}.log"
+        handler = TimedRotatingFileHandler(suffixed_file_name, when="MIDNIGHT")
+        handler.namer = lambda name: f"{name.replace(suffixed_file_name, self.file_name)}.log"
         handler.setLevel(self.level)
-        handler.setFormatter(formatter)
+        handler.setFormatter(CustomJsonFormatter('(level):(name)'))
 
         # create a logger and add the file handler to it
         logger = logging.getLogger(self.name)
-        logger.setLevel(self.level)
         logger.addHandler(handler)
+
+        logger.setLevel(self.level)
 
         return logger
