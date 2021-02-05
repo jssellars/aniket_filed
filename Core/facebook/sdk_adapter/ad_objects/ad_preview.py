d4f365a9199d38f6bc0c00bf0ad2773fff4fff34
Alin Mihaila <Alin@filed.com>
2021-02-05 10:02:52 +0000

    Merged PR 5915: DEV-2395: Adjust catalogs and validations
    🔧 Adjust catalogs and validations
    - To Audience Network add: AdFormat.MOBILE_MEDIUM_RECTANGLE,
    - To Facebook Feed remove: MOBILE_FEED_BASIC, WATCH_FEED_MOBILE
    - To DESKTOP_FEED_STANDARD and MOBILE_FEED_STANDARD to have Facebook in front of the display name.

--- Core/facebook/sdk_adapter/ad_objects/ad_preview.py
+++ Core/facebook/sdk_adapter/ad_objects/ad_preview.py
@@ -19,7 +19,7 @@ class AdFormat(Enum):
     )
     AUDIENCE_NETWORK_OUTSTREAM_VIDEO = Cat(_format.audience_network_outstream_video)
     AUDIENCE_NETWORK_REWARDED_VIDEO = Cat(_format.audience_network_rewarded_video)
-    DESKTOP_FEED_STANDARD = Cat(_format.desktop_feed_standard, display_name="Desktop Feed")
+    DESKTOP_FEED_STANDARD = Cat(_format.desktop_feed_standard, display_name="Facebook Desktop Feed")
     FACEBOOK_STORY_MOBILE = Cat(_format.facebook_story_mobile, display_name="Facebook Story")
     INSTAGRAM_EXPLORE_CONTEXTUAL = Cat(_format.instagram_explore_contextual)
     INSTAGRAM_EXPLORE_IMMERSIVE = Cat(_format.instagram_explore_immersive)
@@ -29,14 +29,14 @@ class AdFormat(Enum):
     INSTANT_ARTICLE_STANDARD = Cat(_format.instant_article_standard)
     INSTREAM_VIDEO_DESKTOP = Cat(_format.instream_video_desktop)
     INSTREAM_VIDEO_MOBILE = Cat(_format.instream_video_mobile)
-    JOB_BROWSER_DESKTOP = Cat(_format.job_browser_desktop)  #  unused in group
-    JOB_BROWSER_MOBILE = Cat(_format.job_browser_mobile)  #  unused in group
+    JOB_BROWSER_DESKTOP = Cat(_format.job_browser_desktop)  # unused in group
+    JOB_BROWSER_MOBILE = Cat(_format.job_browser_mobile)  # unused in group
     MARKETPLACE_MOBILE = Cat(_format.marketplace_mobile)
     MESSENGER_MOBILE_INBOX_MEDIA = Cat(_format.messenger_mobile_inbox_media)
     MESSENGER_MOBILE_STORY_MEDIA = Cat(_format.messenger_mobile_story_media)
     MOBILE_BANNER = Cat(_format.mobile_banner)
     MOBILE_FEED_BASIC = Cat(_format.mobile_feed_basic)
-    MOBILE_FEED_STANDARD = Cat(_format.mobile_feed_standard)
+    MOBILE_FEED_STANDARD = Cat(_format.mobile_feed_standard, display_name="Facebook Mobile News Feed")
     MOBILE_FULLWIDTH = Cat(_format.mobile_fullwidth)  # uncertain inclusion in group
     MOBILE_INTERSTITIAL = Cat(_format.mobile_interstitial)
     MOBILE_MEDIUM_RECTANGLE = Cat(_format.mobile_medium_rectangle)
