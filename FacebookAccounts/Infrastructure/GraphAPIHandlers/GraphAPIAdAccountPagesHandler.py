2822d3b3e68798e40611ba5ede4b5f0b3928bde0
Rahul Rudragoudar <rahul@filed.com>
2021-02-11 13:22:11 +0000

    Merged PR 5968: DEV-2780 FacebookAccounts - AdAccountPages
    Swap http calls to sdk
    Purge associated http helper classes and functions

--- FacebookAccounts/Infrastructure/GraphAPIHandlers/GraphAPIAdAccountPagesHandler.py
+++ FacebookAccounts/Infrastructure/GraphAPIHandlers/GraphAPIAdAccountPagesHandler.py
@@ -1,61 +1,59 @@
 import typing
+from itertools import chain
 
-from Core.Web.FacebookGraphAPI.GraphAPI.GraphAPIClientBase import GraphAPIClientBase
-from Core.Web.FacebookGraphAPI.GraphAPI.GraphAPIClientConfig import GraphAPIClientBaseConfig
+from facebook_business.adobjects.adaccount import AdAccount
+from facebook_business.adobjects.business import Business
+from Core.Web.FacebookGraphAPI.GraphAPI.GraphAPISdkBase import GraphAPISdkBase
 from FacebookAccounts.Infrastructure.GraphAPIDtos.GraphAPIBusinessDto import GraphAPIBusinessDto
-from FacebookAccounts.Infrastructure.GraphAPIDtos.GraphAPIPagesDto import GraphAPIPagesDto
 from FacebookAccounts.Infrastructure.GraphAPIMappings.GraphAPIAdAccountBusinessMapping import \
     GraphAPIAdAccountBusinessMapping
-from FacebookAccounts.Infrastructure.GraphAPIMappings.GraphAPIAdAccountPagesMapping import \
-    GraphAPIAdAccountPagesMapping
-from FacebookAccounts.Infrastructure.GraphAPIRequests.GraphAPIRequestBusiness import GraphAPIRequestBusiness
-from FacebookAccounts.Infrastructure.GraphAPIRequests.GraphAPIRequestClientPages import \
-    GraphAPIRequestClientPages
-from FacebookAccounts.Infrastructure.GraphAPIRequests.GraphAPIRequestOwnedPages import GraphAPIRequestOwnedPages
-from FacebookAccounts.Infrastructure.GraphAPIRequests.GraphAPIRequestPromotePages import \
-    GraphAPIRequestPromotePages
 
 
 class GraphAPIAdAccountPagesHandler:
     @classmethod
-    def handle(cls,
+    def handle(
+        cls,
         permanent_token: typing.AnyStr,
         account_id: typing.AnyStr,
         config
     ) -> typing.List[typing.Dict]:
+
+        _ = GraphAPISdkBase(config.facebook, permanent_token)
+
         # get business_id for ad account
         business_mapping = GraphAPIAdAccountBusinessMapping(GraphAPIBusinessDto)
-        api_config = GraphAPIClientBaseConfig()
-        api_config.request = GraphAPIRequestBusiness(api_version=config.facebook.api_version,
-                                                 access_token=permanent_token,
-                                                 account_id=account_id)
-        graph_api_client = GraphAPIClientBase(business_owner_permanent_token=permanent_token, config=api_config)
-        response, _ = graph_api_client.call_facebook()
+
+        account = AdAccount(account_id)
+        response = account.api_get(fields=["business"])
+
         business = business_mapping.load(response)
 
+        return cls.get_all_pages(account_id=account_id, business_id=business.id)
+
+    @classmethod
+    def get_all_pages(cls, account_id: typing.AnyStr, business_id: typing.AnyStr):
+
+        account = AdAccount(account_id)
+        business = Business(business_id)
+
         # get owned pages
-        pages_mapping = GraphAPIAdAccountPagesMapping(GraphAPIPagesDto)
-        api_config.request = GraphAPIRequestOwnedPages(api_version=config.facebook.api_version,
-                                                   access_token=permanent_token,
-                                                   business_id=business.id)
-        graph_api_client.config = api_config
-        response, _ = graph_api_client.call_facebook()
-        owned_pages = pages_mapping.load(response)
+        owned_pages = business.get_owned_pages()
 
         # get client pages
-        api_config.request = GraphAPIRequestClientPages(api_version=config.facebook.api_version,
-                                                    access_token=permanent_token,
-                                                    business_id=business.id)
-        response, _ = graph_api_client.call_facebook()
-        client_pages = pages_mapping.load(response)
+        client_pages = business.get_client_pages()
 
         # get promoted pages
-        api_config.request = GraphAPIRequestPromotePages(api_version=config.facebook.api_version,
-                                                     access_token=permanent_token,
-                                                     account_id=account_id)
-        response, _ = graph_api_client.call_facebook()
-        promote_pages = pages_mapping.load(response)
+        promote_pages = account.get_promote_pages()
 
-        pages = owned_pages.pages + client_pages.pages + promote_pages.pages
+        all_pages = chain(owned_pages, client_pages, promote_pages)
+
+        # Map and remove duplicates
+        ids = set()
+        pages = []
+        for page in all_pages:
+            if not page["id"] in ids:
+                ids.add(page["id"])
+                page["facebook_id"] = page.pop("id")
+                pages.append(page)
 
         return pages
