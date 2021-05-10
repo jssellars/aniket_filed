06e7c5e99a4974b09e9b4ed0522e8c66d884c45f
Lokendra Singh <lokendra@filed.com>
2021-05-10 16:31:37 +0530

    add: pydantic schema for serialization

---
+++ FiledInfluencer/Api/schemas.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from pydantic import BaseModel, Json
+from pydantic.schema import datetime
+
+
+# https://pydantic-docs.helpmanual.io/usage/schema/#json-schema-types
+class FiledInfluencersPydantic(BaseModel):
+    """
+    Convert SQLAlchemy model to Pydantic Model
+
+    FastAPI uses pydantic model
+    And pydantic models are required for serialization of data
+    """
+    Id: int
+    UpdatedAt: Optional[datetime]
+    UpdatedById: Optional[int]
+    UpdatedByFirstName: Optional[str]
+    UpdatedByLastName: Optional[str]
+    CreatedAt: Optional[datetime]
+    CreatedById: int
+    CreatedByFirstName: Optional[str]
+    CreatedByLastName: Optional[str]
+    Name: str
+    Biography: str
+    Engagement: str
+    Details: Json
+    FiledPlatformId: int
+    InfluencersCategoriesId: Optional[int]
+
+    class Config:
+        orm_mode = True
+
+
+class InfluencersCategoriesPydantic(BaseModel):
+    Id: int
+    Name: str
+
+    class Config:
+        orm_mode = True
