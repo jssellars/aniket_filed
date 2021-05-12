24d42dc4675111e75e374112768f075a0e1e67ca
Lokendra Singh <lokendra@filed.com>
2021-05-12 12:56:53 +0530

    dataclass: provide default value for Optional fields

--- FiledInfluencer/Api/models.py
+++ FiledInfluencer/Api/models.py
@@ -23,12 +23,12 @@ class CategoryHashtag:
 @dataclass
 class FiledInfluencer:
     _id: int
-    filed_platform_id: Optional[int]
-    filed_category_id: Optional[int]
     name: str
     biography: str
     details: Dict[str, Union[str, Dict[str, int]]]
     engagement: Optional[str] = None
+    filed_platform_id: Optional[int] = None
+    filed_category_id: Optional[int] = None
 
 
 @dataclass
@@ -40,10 +40,10 @@ class Platform:
 
 @dataclass
 class InfluencerPost:
-    filed_platform_id: Optional[int]
-    influencer_id: Optional[int]
     post_content: str
     posted_at: str
+    filed_platform_id: Optional[int] = None
+    influencer_id: Optional[int] = None
     engagement: Optional[int] = None
 
 
