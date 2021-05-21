19bb93789a63c2ffe09044b66e53e8915eab263c
Dhananjay Jindal <dhananjay@filed.com>
2021-05-21 17:06:01 +0530

    Dev fix bid strategy (#71)
    * Set fixed value for bid strategy
    
    * :bulb: Remove unnecessary TODO
    
    * :bug: Add fix for bid control
    
    Implementation for DEV-4550

--- FacebookCampaignsBuilder/BackgroundTasks/request_handlers.py
+++ FacebookCampaignsBuilder/BackgroundTasks/request_handlers.py
@@ -1134,10 +1134,12 @@ class AddStructuresToParent:
         # If Parent Campaign has CBO, then don't add adset budget
         if parent_campaign_not_has_cbo and ("budget_optimization" in adset_request):
             budget_opt = adset_request.get("budget_optimization")
-            # TODO: Discuss proper mapping of bidAmount and bidControl wit FE
-            ad_set_template.bid_amount = int(budget_opt.get("bid_control", 0)) * 100
-            # Bid Strategy set to default for FE
-            ad_set_template.bid_strategy = Campaign.BidStrategy.lowest_cost_without_cap;
+            bid_control = int(budget_opt.get("bid_control", 0))
+            if bid_control:
+                ad_set_template.bid_amount = bid_control * 100
+                ad_set_template.bid_strategy = Campaign.BidStrategy.lowest_cost_with_bid_cap
+            else:
+                ad_set_template.bid_strategy = Campaign.BidStrategy.lowest_cost_without_cap
             ad_set_template.set_budget_opt(budget_opt["amount"], budget_opt["budget_allocated_type_id"])
 
         targeting_request = adset_request.get("targeting")
