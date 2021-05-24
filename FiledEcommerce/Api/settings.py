4960fc89f1f60f8568c9a5da5d100c7a092b2a6d
Rahul Rudragoudar <rr83019@gmail.com>
2021-05-24 22:31:07 +0530

    Update core files

--- FiledEcommerce/Api/settings.py
+++ FiledEcommerce/Api/settings.py
@@ -1,4 +1,5 @@
 from Core import settings as core
+from Core.settings import get_env_model
 
 env = core.get_environment()
 
@@ -7,3 +8,7 @@ class Default:
     # WARNING: this must not inherit BaseModel and must be name "Default"
     name = core.Name(domain="filed", name="ecommerce", kind="api")
     port = 47650
+
+    sql_server = core.replace_in_class(get_env_model(env, "sql_server"), name="Dev3.Filed.ProductCatalogs2")
+
+    mongo = core.replace_in_class(get_env_model(env, "mongo"), ecommerce_database_name="dev3_ecommerce")
