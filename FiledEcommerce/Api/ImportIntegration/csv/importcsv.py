0496c87777cf8a73d5c1b2531b7b3098e218b1ce
Toluwalemi <toluwalemisrael@gmail.com>
2021-06-01 15:59:18 +0100

    feat:Add csv integration

---
+++ FiledEcommerce/Api/ImportIntegration/csv/importcsv.py
@@ -0,0 +1,19 @@
+from FiledEcommerce.Api.ImportIntegration.interface.ecommerce import Ecommerce
+
+
+class ImportCsv(Ecommerce):
+    @classmethod
+    def pre_install(cls):
+        pass
+
+    @classmethod
+    def app_install(cls):
+        pass
+
+    @classmethod
+    def app_load(cls):
+        pass
+
+    @classmethod
+    def app_uninstall(cls):
+        pass
