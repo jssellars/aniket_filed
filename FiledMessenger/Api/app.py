908cc5c5838d6db726bbb2567fc5f4c552d670d5
lokendrasingh-filed <82365696+lokendrasingh-filed@users.noreply.github.com>
2021-06-18 11:57:55 +0530

    move: MessengerSocket to app.py (#210)

--- FiledMessenger/Api/app.py
+++ FiledMessenger/Api/app.py
@@ -13,7 +13,7 @@ import flask_restful
 import flask_socketio
 
 from Core.logging_config import request_as_log_dict
-from FiledMessenger.Api import MessengerSocket, routers
+from FiledMessenger.Api import routers
 from FiledMessenger.Api.startup import config
 
 app = flask.Flask(__name__)
@@ -21,7 +21,8 @@ app.url_map.strict_slashes = False
 cors = flask_cors.CORS(app, resources={r"/api/*": {"origins": "*"}})
 api = flask_restful.Api(app)
 async_mode = None
-MessengerSocket.socketio = flask_socketio.SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
+
+socketio = flask_socketio.SocketIO(app, async_mode=async_mode, cors_allowed_origins="*")
 
 router_route_pairs = (
     (routers.HealthCheck, "healthcheck"),
@@ -35,17 +36,17 @@ for router, route in router_route_pairs:
     api.add_resource(router, f"{config.base_url.lower()}/{route}")
 
 namespace_chat = f"{config.base_url.lower()}/chat"
-MessengerSocket.socketio.on_namespace(routers.SocketChat(namespace_chat))
+socketio.on_namespace(routers.SocketChat(namespace_chat))
 
 
 # Unfortunately, namespace based error handling is not available, yet :)
-@MessengerSocket.socketio.on_error(namespace=namespace_chat)
+@socketio.on_error(namespace=namespace_chat)
 def chat_error_handler(e):
     routers.logger.exception(repr(e), extra=request_as_log_dict(flask.request))
 
 
 if __name__ == "__main__":
-    MessengerSocket.socketio.run(
+    socketio.run(
         app=app,
         debug=config.logger_level == "DEBUG",
         host="localhost",
