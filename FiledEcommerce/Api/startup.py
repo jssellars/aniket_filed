8073c9bb03ece70b3d96aad987a3fb052048edae
shailendra-filed <84766139+shailendra-filed@users.noreply.github.com>
2021-06-22 23:59:57 +0530

    fb config for demo (#234)
    Co-authored-by: Shailendra Singh <shailendra964@gmail.com>

--- FiledEcommerce/Api/startup.py
+++ FiledEcommerce/Api/startup.py
@@ -7,6 +7,10 @@ fb_config = fb_settings.core.get_settings(fb_settings, settings.core.get_environ
 fb_fixtures = fixtures.Fixtures(fb_config)
 
 config = settings.core.get_settings(settings, settings.core.get_environment())
+#only for demo, will revert back later
+config.facebook.app_id = "887233235475756"
+config.facebook.app_secret = "2a62adbcb477f3dc95c043b3546a42ea"
+
 fixtures = fixtures.Fixtures(config)
 logging_config.init(config)
 logger = logging_config.get_logger(__name__)
